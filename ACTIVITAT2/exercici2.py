493f709bc0bb078720c589a0b88521d42fed7d49
Brandy-K <146661558+Brandy-K@users.noreply.github.com>
2024-09-25 19:26:01 +0200

    Update exercici2.py

--- ACTIVITAT2/exercici2.py
+++ ACTIVITAT2/exercici2.py
@@ -1,1 +1,7 @@
-
+value = float(input("valor en €: "))
+while True:
+    vat = float(input("VAT (4, 10, or 21): "))
+    if vat==4 or vat==10 or vat==21:
+     print(value+value*vat/100)
+else:
+    print("enter a valid VAT.")
