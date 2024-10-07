5380275310a43c6f462dfe36a085733388234481
brandy <2023_brandy.sherile@iticbcn.cat>
2024-10-07 16:50:07 +0200

    init commit

--- ACTIVITAT3/exercici10.py
+++ ACTIVITAT3/exercici10.py
@@ -1,7 +1,8 @@
 import string
 
-alphabets = list(string.ascii_lowercase)  #to store alphabets
-
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+             't', 'u', 'v', 'w', 'x', 'y', 'z'] #to store alphabets
+'''list(string.ascii_lowercase)'''
 nova_llista = []  #Llista para gaurdar les lletres que no estan en la position multiples de 3
 
 for i in range(len(alphabets)):
