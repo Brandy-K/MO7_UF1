9753c6e6934881fd24085f2be9039e4238af0ba0
Brandy <2023_brandy.sherile@iticbcn.cat>
2024-10-31 22:24:49 +0100

    main.py updated

--- ACTIVITAT_7/main.py
+++ ACTIVITAT_7/main.py
@@ -1,4 +1,3 @@
-# Main to call all the functions
 import create
 import create_table as ct
 import read
@@ -7,19 +6,30 @@ import delete
 
 
 def main():
-    # Call the function for creating the table
-    ct.create_table()
-    # Call the function for inserting data into the table
-    create.insert_records(5, "Brandy", "DAW", "FP", "2A", "A01")
-    create.insert_records(1, "Bruno", "DAW", "FP", "2A", "A01")
-    create.insert_records(3, "Peter", "DAM", "FP", "2B", "A02")
-    create.insert_records(4, "Doua", "ASIX", "FP", "2A", "A05")
-    # Call the function for reading the data
-    read.read_table()
-    # Call the function for updating the data
-    update.update_table(1,"Sherile")
-    # Call function for deleting data
+    try:
+        # Call the function for creating the table
+        ct.create_table()
+
+        # Call the function for inserting data into the table
+        create.insert_records(5, "Brandy", "DAW", "FP", "2A", "A01")
+        create.insert_records(1, "Bruno", "DAW", "FP", "2A", "A01")
+        create.insert_records(3, "Peter", "DAM", "FP", "2B", "A02")
+        create.insert_records(4, "Doua", "ASIX", "FP", "2A", "A05")
+
+        # Call the function for reading the data
+        read.read_table()
+
+        # Call the function for updating the data
+        update.update_table(1, "Sherile")
+
+        # Call the function for deleting data
+        delete.delete_data(1)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        print("Program finished executing.")
 
 
-if __name__ == "__main__":
-    main()  # To start the program
+# call the main function
+main()
